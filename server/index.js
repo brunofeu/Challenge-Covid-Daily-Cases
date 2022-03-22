b8f36d5f9eafa59e585ad4bfa1c5e44f34c31d1a
brunofeu <brunofeu89@gmail.com>
2022-03-22 17:58:56 -0300

    create routes

--- server/index.js
+++ server/index.js
@@ -9,4 +9,8 @@ app.get('/', (_req, res) => {
   res.status(200).json({message: 'Fullstack Challenge 2021 🏅 - Covid Daily Cases'});
 });
 
+// app.get('/cases/:date/count', (_req, res) => {})
+// app.get('/cases/:date/cumulative', (_req, res) => {})
+// app.get('/dates', (_req, res) => {})
+
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
