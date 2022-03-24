792a8cd01b2f31eaef35b05a56d186e108eb2d34
brunofeu <brunofeu89@gmail.com>
2022-03-24 15:52:07 -0300

    map working on component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import MapChart from './components/MapChart';
@@ -9,10 +10,10 @@ function App() {
 
 
   return (
-    <div>
-      {/* <Header/> */}
-      {/* <Main /> */}
-      <MapChart /> 
+    <div >
+      <Header />
+      <MapChart />
+      <Footer />
     </div>
   );
 }
--- client/src/components/MapChart.jsx
+++ client/src/components/MapChart.jsx
@@ -1,51 +1,52 @@
-import {Chart} from 'chart.js';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import * as ChartGeo from 'chartjs-chart-geo'
-import { useEffect } from 'react'
+import React, {useEffect, useState} from "react";
+import { VectorMap } from "react-jvectormap";
+import { getData } from "country-list";
 
-const MapChart = () =>{
 
-    useEffect(()=>{
 
-        let canvas = document.getElementById("canvas")
-        if(!canvas) return
-
-        fetch('https://unpkg.com/world-atlas/countries-50m.json').then((r) => r.json()).then((data) => {
-          const countries = ChartGeo.topojson.feature(data, data.objects.countries).features;
-
-            const chart = new Chart(canvas.getContext("2d"), {
-              type: 'choropleth',
-              plugins: [ChartDataLabels],
-              data: {
-                labels: countries.map((d) => d.properties.name),
-                datasets: [{
-                  label: 'Countries',
-                  data: countries.map((d) => ({feature: d, value: Math.random()})),
-                }]
-              },
-              options: {
-                showOutline: false,
-                showGraticule: false,
-                plugins: {
-                  legend: {
-                    display: false
-                  },
-                },
-                scales: {
-                  xy: {
-                    projection: 'equalEarth'
-                  }
-                }
-              }
-            });
-          });
-    },)
-    
-    return(
-        <div>
-            <canvas id='canvas'></canvas>
-        </div>
-    )
+function MapChart() {
+  return (
+    <div className='map-container'>
+      <VectorMap
+      map={"world_mill"}
+      zoomOnScroll={true}
+      // onRegionClick={null}
+      backgroundColor='black'
+      containerClassName="map"
+      regionStyle={{
+        initial: {
+          fill: "#e4e4e4",
+          "fill-opacity": 0.9,
+          stroke: "none",
+          "stroke-width": 0,
+          "stroke-opacity": 0,
+          cursor: "pointer"
+        },
+        hover: { 
+          "fill-opacity": 0.8,
+          cursor: "pointer"
+        },
+        selected: {
+          fill: "#212121",
+          stroke: "#212121",
+          "stroke-width": 1,
+        },
+        selectedHover: {
+          "fill-opacity": 0.8,
+        }
+      }}
+      series={{
+        regions: [
+          {
+            // values: countryCodeStateObj,
+            scale: ["#5606A5", "#E5D1F9"],
+            normalizeFunction: "polynomial"
+          }
+        ]
+      }}
+    />
+    </div>
+  )
 }
 
-    export default MapChart;
+export default MapChart
