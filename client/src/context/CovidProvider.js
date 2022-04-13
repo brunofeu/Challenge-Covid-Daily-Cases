8412f3475e648f5276d2e3e2c52cdf05799bf1ba
brunofeu <brunofeu89@gmail.com>
2022-04-13 18:51:26 -0300

    use axios to request

--- client/src/context/CovidProvider.js
+++ client/src/context/CovidProvider.js
@@ -4,18 +4,11 @@ const axios = require('axios');
 
 function CovidProvider({ children }) {
 
-  const BASE_URL = 'https://challenge-covid-daily-cases.herokuapp.com/'
+  const BASE_URL = 'https://challenge-covid-daily-cases.herokuapp.com'
 
   const http = axios.create({baseURL: BASE_URL, timeout: 30000})
 
-  axios({
-    method: 'get',
-    url: BASE_URL,
-    responseType: 'stream'
-  })
-    .then(function (response) {
-      console.log(response)
-    });
+  
 
   const [cumulative, setCumulative] = useState('count')
   const [dates, setDates] = useState([])
@@ -35,20 +28,24 @@ function CovidProvider({ children }) {
 
   useEffect(() => {
 
-    http.get('/dates')
-      .then((res) => res.json())
-      .then((response) => {
-        setDates(response)
-        setDateSelected(response[0].date)
-      })
-  }, [http]);
+    axios({
+      method: 'get',
+      url: `${BASE_URL}/dates`,
+    }).then(({data}) => {
+        setDates(data)
+        setDateSelected(data[0].date)
+      });
+
+  }, []);
 
   useEffect(() => {
-    // fetch(`${BASE_URL}/cases/${dateSelected}/${cumulative}`)
-    http.get(`/cases/${dateSelected}/${cumulative}`)
-      .then((res) => res.json())
-      .then((response) => setCases(response));
-  }, [dateSelected, cumulative, http]);
+
+    axios({
+      method: 'get',
+      url: `${BASE_URL}/cases/${dateSelected}/${cumulative}`,
+    }).then(({data}) => setCases(data));
+    
+  }, [dateSelected, cumulative]);
 
   return (
     <CovidContext.Provider value={ contextValue }>
